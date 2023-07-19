c7a42ae577d57c4b67d80369bee837158c00520c
RyanDodd21 <74713007+RyanDodd21@users.noreply.github.com>
2023-07-19 18:32:35 +0100

    V1.0

--- GaTS.py
+++ GaTS.py
@@ -3,45 +3,133 @@ import shutil
 import os
 import sys
 
+# Text for the -h or -help switches
+logo = """
+    _____      ____     ________    _____  
+   / ___ \    (    )   (___  ___)  / ____\ 
+  / /   \_)   / /\ \       ) )    ( (___   
+ ( (  ____   ( (__) )     ( (      \___ \  
+ ( ( (__  )   )    (       ) )         ) ) 
+  \ \__/ /   /  /\  \     ( (      ___/ /  
+   \____/   /__(  )__\    /__\    /____/  
 
-'Check there are arguments on the script'
+        Gimmie all Tools and Scripts
+"""
+help_text = """
+-all     Download all Tools
+-w       Download all Windows Tools
+-we      Download Windows Enumeration Scripts
+-wp      Download Windows PrivEsc Exploits
+-l       Download all Linux Tools
+-le      Download Linux Enumeration
+-lp      Download Linux PrivEsc Exploits
+
+Please only use 1 switch at a time.
+
+Example: python3 GaTS.py -all | python3 GaTS.py -lp
+"""
+
 args = sys.argv
+
 if len(args) < 2:
-    print("Usage: python3 MaSD.py -help. Example python3 MaSD.py -w -le")
+    print("Usage: python3 GaTS.py -help. Example python3 GaTS.py -w")
     sys.exit(1)
-args.pop(0)
-
-DownloadAll = "-all" in args
-
-if any(option in args for option in ["-h", "-help"]):
-    print("GaTS - Gimmie all Tools and Scripts")
-    print("-all     Download all Tools")
-    print("-w       Download all Windows Tools")
-    print("-we      Download Windows Enumeration Tools")
-    print("-wp      Download Windows PrivEsc Tools")
-    print("-l       Download al Linux Tools")
-    sys.exit(0)
-
-'GitHub URLS and associated names'
-WindowsURL = [ {"url": "https://github.com/ParrotSec/mimikatz.git", "name": "mimikatz"},
-               {"url": "https://github.com/411Hall/JAWS.git", "name": "JAWS"},
-               {"url": "https://github.com/bitsadmin/wesng.git", "name": "Windows Exploit Suggester NG"}
-               ]
-WindowsNames = ["mimkatz", "JAWS", "Windows Exploit Suggester NG"]
-LinuxURL = []
-LinuxNames = []
 
 
+category = args[1]
+specific_tool = args[2] if len(args) >= 3 else None
+
+if category == "-h" or category =="-help":
+        print(logo)
+        print(help_text)
+        sys.exit(1)
+
+# GitHub URLS and associated names
+WindowsEnumTools = [
+    {"url": "https://github.com/411Hall/JAWS.git", "name": "JAWS"},
+    {"url": "https://github.com/bitsadmin/wesng.git", "name": "Windows Exploit Suggester NG"},
+    {"url": "https://github.com/PowerShellMafia/PowerSploit.git", "name": "PowerSploit"},
+    {"url": "https://github.com/Arvanaghi/SessionGopher.git", "name": "Session Gopher"},
+]
+
+WindowsPrivEscExploits = [
+    {"url": "https://github.com/ParrotSec/mimikatz.git", "name": "mimikatz"},
+    {"url": "https://github.com/CCob/SweetPotato.git", "name": "Sweet Potato"},
+    {"url": "https://github.com/rasta-mouse/Watson.git", "name": "Watson"},
+    {"url": "https://github.com/pentestmonkey/windows-privesc-check.git", "name": "Windows-Privesc_Check"},
+]
+
+LinuxEnumTools = [
+    {"url": "https://github.com/rebootuser/LinEnum.git", "name": "LinEnum"},
+]
+
+LinuxPrivEscExploits = [
+    {"url": "https://github.com/evait-security/ClickNRoot.git", "name": "Click_n_root"},
+]
+
+# Function used to clone repository
 def clone_repository(repo_url, destination_path):
     command = ['git', 'clone', repo_url, destination_path]
     subprocess.run(command, check=True)
 
-name = 0
-if DownloadAll or "-w" in args:
-    for url in WindowsURL:
-        destination = f"./GaTS/Windows/{url['name']}"
-        if os.path.exists(destination):
-            print(f"Removing existing directory: {destination}")
-            shutil.rmtree(destination)
-        clone_repository(url['url'], destination)
-        name += 1
+def check_directory_exists(destination):
+    if os.path.exists(destination):
+        print(f"Removing existing directory: {destination}")
+        shutil.rmtree(destination)
+
+def download_tools(tool_list, destination_prefix):
+    for Tool in tool_list:
+        destination = f"./{destination_prefix}/{Tool['name']}"
+        check_directory_exists(destination)
+        clone_repository(Tool['url'], destination)
+
+# Handle the command-line arguments for specific tools
+print(logo)
+if specific_tool:
+    if specific_tool in ["-we", "-wp", "-le", "-lp"]:
+        if len(args) >= 4:
+            specific_tool = specific_tool + args[3]
+        else:
+            print("Invalid specific tool option.")
+            sys.exit(1)
+
+        if "-we" in specific_tool:
+            download_tools(WindowsEnumTools, "Windows/Enumeration")
+        if "-wp" in specific_tool:
+            download_tools(WindowsPrivEscExploits, "Windows/Privilege Escalation")
+        if "-le" in specific_tool:
+            download_tools(LinuxEnumTools, "Linux/Enumeration")
+        if "-lp" in specific_tool:
+            download_tools(LinuxPrivEscExploits, "Linux/Privilege Escalation")
+
+# Handle the command-line arguments for categories
+else:
+    if category == "-all":
+        download_tools(WindowsEnumTools, "Windows/Enumeration")
+        download_tools(WindowsPrivEscExploits, "Windows/Privilege Escalation")
+        download_tools(LinuxEnumTools, "Linux/Enumeration")
+        download_tools(LinuxPrivEscExploits, "Linux/Privilege Escalation")
+    elif category == "-w":
+        download_tools(WindowsEnumTools, "Windows/Enumeration")
+        download_tools(WindowsPrivEscExploits, "Windows/Privilege Escalation")
+    elif category == "-l":
+        download_tools(LinuxEnumTools, "Linux/Enumeration")
+        download_tools(LinuxPrivEscExploits, "Linux/Privilege Escalation")
+
+    elif category in ["-we", "-wp", "-le", "-lp"]:
+        if len(args) >= 3:
+            category = category + args[2]
+
+        if "-we" in category:
+            download_tools(WindowsEnumTools, "Windows/Enumeration")
+        if "-wp" in category:
+            download_tools(WindowsPrivEscExploits, "Windows/Privilege Escalation")
+        if "-le" in category:
+            download_tools(LinuxEnumTools, "Linux/Enumeration")
+        if "-lp" in category:
+            download_tools(LinuxPrivEscExploits, "Linux/Privilege Escalation")
+    else:
+        print("Input not valid, Use -h or -help")
+        sys.exit(1)
+
+download_tools("https://github.com/carlospolop/PEASS-ng.git", "./")
--- README.md
+++ README.md
@@ -1,5 +1,46 @@
-# GaTS - Gimmie all the Tools and Scripts,
+    _____      ____     ________    _____  
+   / ___ \    (    )   (___  ___)  / ____\ 
+  / /   \_)   / /\ \       ) )    ( (___   
+ ( (  ____   ( (__) )     ( (      \___ \  
+ ( ( (__  )   )    (       ) )         ) ) 
+  \ \__/ /   /  /\  \     ( (      ___/ /  
+   \____/   /__(  )__\    /__\    /____/  
 
-
+        Gimmie all Tools and Scripts
 
 Work in Progress Script to download a large collection of Windows and Linux enumiration and privEsc scripts 
+
+
+Available commands 
+
+-all     Download all Tools
+-w       Download all Windows Tools
+-we      Download Windows Enumeration Scripts
+-wp      Download Windows PrivEsc Exploits
+-l       Download all Linux Tools
+-le      Download Linux Enumeration
+-lp      Download Linux PrivEsc Exploits
+
+Please only use 1 switch at a time. (Work In Progress)
+
+Example: python3 GaTS.py -all | python3 GaTS.py -lp
+
+Please feel free to suggest more scripts and tools to add to this.
+
+List of Current Tools and Respective Page:
+------------------------------------------
+| Tool Name              | Repository Link                                       |
+|------------------------|-------------------------------------------------------|
+| JAWS                   | [https://github.com/411Hall/JAWS.git](https://github.com/411Hall/JAWS.git) |
+| WESNG                  | [https://github.com/bitsadmin/wesng.git](https://github.com/bitsadmin/wesng.git) |
+| PowerSploit            | [https://github.com/PowerShellMafia/PowerSploit.git](https://github.com/PowerShellMafia/PowerSploit.git) |
+| SessionGopher          | [https://github.com/Arvanaghi/SessionGopher.git](https://github.com/Arvanaghi/SessionGopher.git) |
+| MimiKatz               | [https://github.com/ParrotSec/mimikatz.git](https://github.com/ParrotSec/mimikatz.git) |
+| SweetPotato            | [https://github.com/CCob/SweetPotato.git](https://github.com/CCob/SweetPotato.git) |
+| Watson                 | [https://github.com/rasta-mouse/Watson.git](https://github.com/rasta-mouse/Watson.git) |
+| Windows-Privsec-Check  | [https://github.com/pentestmonkey/windows-privesc-check.git](https://github.com/pentestmonkey/windows-privesc-check.git) |
+| LinEnum                | [https://github.com/rebootuser/LinEnum.git](https://github.com/rebootuser/LinEnum.git) |
+| Click n root           | [https://github.com/evait-security/ClickNRoot.git](https://github.com/evait-security/ClickNRoot.git) |
+| PEASS-NG               | [https://github.com/carlospolop/PEASS-ng.git](https://github.com
+
+I do not own any of these tools and all support should be directed at the respective tool.
